7754b7f875bff15f996afc91a326227e931c38a8
Rizky Ivan Darmawan <ivanriski007@gmail.com>
2023-03-03 08:08:14 +0700

    Latest update

--- src/lib/components/general/SideLeft.svelte
+++ src/lib/components/general/SideLeft.svelte
@@ -21,7 +21,7 @@
 </script>
 <aside class="absolute inset-y-0 left-2 md:left-4 hidden md:flex flex-col justify-center items-center bg-transparent w-36 h-screen z-10">
     <div class="flex flex-col justify-center items-center w-full h-20 max-h-xl space-y-2 rounded-lg bg-transparent mb-4 md:mb-8">
-        <button class="flex flex-row items-center space-x-4 w-36 h-auto px-4 bg-zinc-100 hover:bg-white">
+        <button on:click={() => (setResolution.set('r1'))} class="flex flex-row items-center space-x-4 w-36 md:w-40 h-auto px-4 {$setResolution === 'r1' ? 'bg-zinc-300 border' : 'bg-zinc-100'} hover:bg-white">
             <div class="flex justify-center items-center w-8 h-8 bg-white shadow">
                 <i class="i-tabler-maximize w-4 h-4"></i>
             </div>
@@ -29,7 +29,7 @@
                 <span class="block text-xs font-bold">300 x 300</span>
             </div>
         </button>
-        <button class="flex flex-row items-center space-x-4 w-36 h-auto px-4 bg-zinc-100 hover:bg-white">
+        <button on:click={() => (setResolution.set('r2'))} class="flex flex-row items-center space-x-4 w-36 md:w-40 h-auto px-4 {$setResolution === 'r2' ? 'bg-zinc-300 border' : 'bg-zinc-100'} hover:bg-white">
             <div class="flex justify-center items-center w-8 h-8 bg-white shadow">
                 <i class="i-tabler-maximize w-4 h-4"></i>
             </div>
@@ -37,7 +37,7 @@
                 <span class="block text-xs font-bold">600 x 600</span>
             </div>
         </button>
-        <button class="flex flex-row items-center space-x-4 w-36 h-auto px-4 bg-zinc-100 hover:bg-white">
+        <button on:click={() => (setResolution.set('r3'), document.body.style.transform = 'scale(0.7)')} class="flex flex-row items-center space-x-4 w-36 md:w-40 h-auto px-4 {$setResolution === 'r3' ? 'bg-zinc-300 border' : 'bg-zinc-100'} hover:bg-white">
             <div class="flex justify-center items-center w-8 h-8 bg-white shadow">
                 <i class="i-tabler-maximize w-4 h-4"></i>
             </div>
@@ -45,7 +45,7 @@
                 <span class="block text-xs font-bold">768 x 1024</span>
             </div>
         </button>
-        <button class="flex flex-row items-center space-x-4 w-36 h-auto px-4 bg-zinc-100 hover:bg-white">
+        <button on:click={() => (setResolution.set('r4'))} class="flex flex-row items-center space-x-4 w-36 md:w-40 h-auto px-4 {$setResolution === 'r4' ? 'bg-zinc-300 border' : 'bg-zinc-100'} hover:bg-white">
             <div class="flex justify-center items-center w-8 h-8 bg-white shadow">
                 <i class="i-tabler-maximize w-4 h-4"></i>
             </div>
@@ -55,9 +55,9 @@
         </button>
     </div>
     <div class="flex flex-col justify-center items-center w-full h-20 max-h-xl space-y-2 rounded-lg bg-transparent">
-        <button on:click={resetClick} class="flex flex-row items-center space-x-4 w-36 h-auto px-4 py-2 bg-red-300 hover:bg-red-400">
+        <button on:click={resetClick} class="flex flex-row items-center space-x-4 w-36 md:w-40 h-auto px-4 py-2 bg-black shadow-xl hover:bg-red-400">
             <div class="flex flex-col leading-none">
-                <span class="block text-xs font-bold">Reset</span>
+                <span class="block text-xs font-bold text-white">Reset</span>
             </div>
         </button>
     </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { SEO } from '$general/index';
     import { websiteSchema, organizationSchema } from '$utils/json-ld';
-    import { setMediaScreen } from '$stores';
+    import { setMediaScreen, setResolution } from '$stores';
 
     $: console.log($setMediaScreen);
 </script>
@@ -21,30 +21,58 @@
 `}>
   {#if $setMediaScreen === 'desktop'}
   <div class="flex flex-col justify-center items-center w-full h-screen bg-zinc-200 border-2 border-zinc-100 shadow-xl bg-transparent overflow-hidden relative">
-    <div class="flex flex-col w-auto h-auto text-center md:text-left">
-      <span class="font-black uppercase leading-tight text-xl tracking-widest">tfmkit</span>
-      <small class="font-thin uppercase leading-tight text-xs tracking-widest">Digital Assets Tools</small>
+    <div class={`flex justify-center items-center rounded-lg bg-zinc-50 shadow-xl p-8
+      ${$setResolution === 'r1' ? 'w-[300px] h-[300px]' : ''}
+      ${$setResolution === 'r2' ? 'w-[600px] h-[600px]' : ''}
+      ${$setResolution === 'r3' ? 'w-[768px] h-[1024px]' : ''}
+      ${$setResolution === 'r4' ? 'w-[1200px] h-[800px]' : ''}
+    `}>
+      <div class="flex flex-col w-auto h-auto text-center md:text-left">
+        <span class="font-black uppercase leading-tight text-xl tracking-widest">tfmkit</span>
+        <small class="font-thin uppercase leading-tight text-xs tracking-widest">Digital Assets Tools</small>
+      </div>
     </div>
   </div>
   {:else if $setMediaScreen === 'tablet'}
   <div class="flex flex-col justify-center items-center w-[820px] h-screen bg-zinc-200 border-2 border-zinc-100 shadow-xl bg-transparent overflow-hidden relative">
-    <div class="flex flex-col w-auto h-auto text-center">
-      <span class="font-black uppercase leading-tight text-xl tracking-widest">tfmkit</span>
-      <small class="font-thin uppercase leading-tight text-xs tracking-widest">Digital Assets Tools</small>
+    <div class={`flex justify-center items-center rounded-lg bg-zinc-50 shadow-xl p-8
+      ${$setResolution === 'r1' ? 'w-[300px] h-[300px]' : ''}
+      ${$setResolution === 'r2' ? 'w-[600px] h-[600px]' : ''}
+      ${$setResolution === 'r3' ? 'w-[768px] h-[1024px]' : ''}
+      ${$setResolution === 'r4' ? 'w-[1200px] h-[800px]' : ''}
+    `}>
+      <div class="flex flex-col w-auto h-auto text-center md:text-left">
+        <span class="font-black uppercase leading-tight text-xl tracking-widest">tfmkit</span>
+        <small class="font-thin uppercase leading-tight text-xs tracking-widest">Digital Assets Tools</small>
+      </div>
     </div>
   </div>
   {:else if $setMediaScreen === 'mobile'}
   <div class="flex flex-col justify-center items-center w-[375px] h-[667px] bg-zinc-200 border-2 border-zinc-100 shadow-xl bg-transparent overflow-hidden relative">
-    <div class="flex flex-col w-auto h-auto text-center">
-      <span class="font-black uppercase leading-tight text-xl tracking-widest">tfmkit</span>
-      <small class="font-thin uppercase leading-tight text-xs tracking-widest">Digital Assets Tools</small>
+    <div class={`flex justify-center items-center rounded-lg bg-zinc-50 shadow-xl p-8
+      ${$setResolution === 'r1' ? 'w-[300px] h-[300px]' : ''}
+      ${$setResolution === 'r2' ? 'w-[600px] h-[600px]' : ''}
+      ${$setResolution === 'r3' ? 'w-[768px] h-[1024px]' : ''}
+      ${$setResolution === 'r4' ? 'w-[1200px] h-[800px]' : ''}
+    `}>
+      <div class="flex flex-col w-auto h-auto text-center md:text-left">
+        <span class="font-black uppercase leading-tight text-xl tracking-widest">tfmkit</span>
+        <small class="font-thin uppercase leading-tight text-xs tracking-widest">Digital Assets Tools</small>
+      </div>
     </div>
   </div>
   {:else}
   <div class="flex flex-col justify-center items-center w-full h-screen bg-zinc-900 border-2 border-zinc-900 shadow-xl bg-transparent overflow-hidden relative">
-    <div class="flex flex-col w-auto h-auto text-center md:text-left">
-      <span class="text-white font-black uppercase leading-tight text-xl tracking-widest">tfmkit</span>
-      <small class="text-white font-thin uppercase leading-tight text-xs tracking-widest">Digital Assets Tools</small>
+    <div class={`flex justify-center items-center rounded-lg bg-zinc-50 shadow-xl p-8
+      ${$setResolution === 'r1' ? 'w-[300px] h-[300px]' : ''}
+      ${$setResolution === 'r2' ? 'w-[600px] h-[600px]' : ''}
+      ${$setResolution === 'r3' ? 'w-[768px] h-[1024px]' : ''}
+      ${$setResolution === 'r4' ? 'w-[1200px] h-[800px]' : ''}
+    `}>
+      <div class="flex flex-col w-auto h-auto text-center md:text-left">
+        <span class="font-black uppercase leading-tight text-xl tracking-widest">tfmkit</span>
+        <small class="font-thin uppercase leading-tight text-xs tracking-widest">Digital Assets Tools</small>
+      </div>
     </div>
   </div>
   {/if}
